67f18603249a07b9207be6fd9f8f0372b01f545c
Raj Srikar <rajsrikar.d161@gmail.com>
2022-09-29 22:00:05 +0530

    Fixed Indentations & Implemented 'text_multiline'

--- codeblock/cpp_generator.js
+++ codeblock/cpp_generator.js
@@ -1,13 +1,68 @@
 const cppGenerator = new Blockly.Generator('CPP');
 
+cppGenerator.ORDER_ATOMIC = 0;
+cppGenerator.ORDER_NEW = 1.1;
+cppGenerator.ORDER_MEMBER = 1.2;
+cppGenerator.ORDER_FUNCTION_CALL = 2;
+cppGenerator.ORDER_INCREMENT = 3;
+cppGenerator.ORDER_DECREMENT = 3;
+cppGenerator.ORDER_BITWISE_NOT = 4.1;
+cppGenerator.ORDER_UNARY_PLUS = 4.2;
+cppGenerator.ORDER_UNARY_NEGATION = 4.3;
+cppGenerator.ORDER_LOGICAL_NOT = 4.4;
+cppGenerator.ORDER_TYPEOF = 4.5;
+cppGenerator.ORDER_VOID = 4.6;
+cppGenerator.ORDER_DELETE = 4.7;
+cppGenerator.ORDER_AWAIT = 4.8;
+cppGenerator.ORDER_EXPONENTIATION = 5;
+cppGenerator.ORDER_MULTIPLICATION = 5.1;
+cppGenerator.ORDER_DIVISION = 5.2;
+cppGenerator.ORDER_MODULUS = 5.3;
+cppGenerator.ORDER_SUBTRACTION = 6.1;
+cppGenerator.ORDER_ADDITION = 6.2;
+cppGenerator.ORDER_BITWISE_SHIFT = 7;
+cppGenerator.ORDER_RELATIONAL = 8;
+cppGenerator.ORDER_IN = 8;
+cppGenerator.ORDER_INSTANCEOF = 8;
+cppGenerator.ORDER_EQUALITY = 9;
+cppGenerator.ORDER_BITWISE_AND = 10;
+cppGenerator.ORDER_BITWISE_XOR = 11;
+cppGenerator.ORDER_BITWISE_OR = 12;
+cppGenerator.ORDER_LOGICAL_AND = 13;
+cppGenerator.ORDER_LOGICAL_OR = 14;
+cppGenerator.ORDER_CONDITIONAL = 15;
+cppGenerator.ORDER_ASSIGNMENT = 16;
+cppGenerator.ORDER_YIELD = 17;
+cppGenerator.ORDER_COMMA = 18;
+cppGenerator.ORDER_NONE = 99;
+cppGenerator.ORDER_OVERRIDES = [
+    [cppGenerator.ORDER_FUNCTION_CALL, cppGenerator.ORDER_MEMBER],
+    [cppGenerator.ORDER_FUNCTION_CALL, cppGenerator.ORDER_FUNCTION_CALL],
+    [cppGenerator.ORDER_MEMBER, cppGenerator.ORDER_MEMBER],
+    [cppGenerator.ORDER_MEMBER,
+        cppGenerator.ORDER_FUNCTION_CALL
+    ],
+    [cppGenerator.ORDER_LOGICAL_NOT, cppGenerator.ORDER_LOGICAL_NOT],
+    [cppGenerator.ORDER_MULTIPLICATION, cppGenerator.ORDER_MULTIPLICATION],
+    [cppGenerator.ORDER_ADDITION, cppGenerator.ORDER_ADDITION],
+    [cppGenerator.ORDER_LOGICAL_AND,
+        cppGenerator.ORDER_LOGICAL_AND
+    ],
+    [cppGenerator.ORDER_LOGICAL_OR, cppGenerator.ORDER_LOGICAL_OR]
+];
+
 function semify(block, stringValue) {
-    return !block.parentBlock_ ? stringValue + ';' : stringValue;
+    if(!block.parentBlock_){
+        stringValue = cppGenerator.prefixLines(stringValue, cppGenerator.INDENT);
+        return stringValue + ';';
+    }
+    return stringValue;
 }
 
 cppGenerator['text'] = function(block) {
     var textValue = block.getFieldValue('TEXT');
     var code = semify(block, `"${textValue}"`);
-    return [code, 0];
+    return [code, cppGenerator.ORDER_ATOMIC];
 };
 
 cppGenerator['text_join'] = function(block) {
@@ -19,5 +74,14 @@ cppGenerator['text_join'] = function(block) {
             values.push(valueCode);
     }
     let valueString = semify(block, values.join(' + '));
-    return [valueString, 0];
-}
+    return [valueString, cppGenerator.ORDER_ATOMIC];
+};
+
+cppGenerator.text_multiline = function(block) {
+    a = block.getFieldValue("TEXT");
+    a = '"' + a + '"';
+    a = a.replaceAll('\n','\\n');
+    var b = -1 !== a.indexOf("+") ? cppGenerator.ORDER_ADDITION : cppGenerator.ORDER_ATOMIC;
+    a = semify(block, a);
+    return [a, b];
+};
